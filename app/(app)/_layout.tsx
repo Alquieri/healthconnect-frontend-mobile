55f711018e933ddaaafa9399748cdeb3e87c870a
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-08 22:06:31 -0300

    fix: fixed logout button

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,15 +1,22 @@
-import { Tabs } from 'expo-router';
+import { Tabs, useRouter } from 'expo-router';
 import { Button } from 'react-native';
 import { useAuth } from '../../src/context/AuthContext';
 import { HeaderLogo } from '../../src/components/HeaderLogo';
 
 export default function AppLayout() {
   const { logout } = useAuth();
+  const router = useRouter();
+
+   const handleLogout = () => {
+    logout(); 
+    
+    router.replace('/login'); 
+  };
 
   return (
     <Tabs
       screenOptions={{
-        headerLeft: () => <Button title="Sair" onPress={logout} />,
+        headerLeft: () => <Button title="Sair" onPress={handleLogout} />,
       }}
     >
       <Tabs.Screen
