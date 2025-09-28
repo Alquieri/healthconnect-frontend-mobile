a6a2599a7abf2d50c09151307b5cea7d5db72182
Alquieri <Guilherme.alquieri@gmail.com>
2025-09-28 18:45:37 -0300

    feat: enhance specialty and user services with better error handling

--- src/api/services/user.tsx
+++ src/api/services/user.tsx
@@ -3,9 +3,7 @@ import { apiPrivate, apiPublic } from '../api';
 import { UserPath } from '../enums/routes'
 import { UserDto } from '../models/user';
 
-
 export async function registerPatient(request: UserDto.RegisterRequest): Promise<UserDto.RegisterResponse> {
-
     try {
         console.log('Registering patient with request:', request);
         const { data: response } = await apiPublic.post<UserDto.RegisterResponse>(UserPath.REGISTER_PATIENT, request);
@@ -13,11 +11,9 @@ export async function registerPatient(request: UserDto.RegisterRequest): Promise
         return response;
     } catch (error: any) {
         if (error instanceof AxiosError) {
-
             const responseData = error.response?.data;
             const errorMessage = responseData?.title || responseData?.message || 'Os dados enviados são inválidos.';
             throw new Error(errorMessage);
-
         } else {
             console.error('Erro inesperado no registro:', error);
             throw new Error('Ocorreu um erro inesperado.');
