fa189f35b7fbcfbf76f60b99af45da965cdb11f7
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-29 17:01:40 -0300

    feat: improved createHeaders

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -7,6 +7,9 @@ import { getToken, deleteToken } from './services/secure-store.service';
 const createHeaders = () => ({
   'Content-Type': 'application/json',
   'ngrok-skip-browser-warning': 'true', 
+  'Cache-Control': 'no-cache, no-store, must-revalidate',
+  'Pragma': 'no-cache',
+  'Expires': '0',
 });
 
 // API Pública (sem token)
